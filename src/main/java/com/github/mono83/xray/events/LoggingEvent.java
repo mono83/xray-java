291142f46383ddc590fde604e489a2a9e01ad774
Gotterdemarung <gotterdemarung@gmail.com>
2019-09-24 15:45:17 +0300

    exceptions and message getters

--- src/main/java/com/github/mono83/xray/events/LoggingEvent.java
+++ src/main/java/com/github/mono83/xray/events/LoggingEvent.java
@@ -1,9 +1,14 @@
 package com.github.mono83.xray.events;
 
 import com.github.mono83.xray.Arg;
+import com.github.mono83.xray.ArgException;
 import com.github.mono83.xray.Args;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.function.BiFunction;
 
 /**
  * Contains logging data.
@@ -22,9 +27,9 @@ public class LoggingEvent {
      */
     public final String logger;
     /**
-     * Message to log.
+     * Message pattern to log.
      */
-    public final String message;
+    public final String pattern;
     /**
      * Logging arguments.
      */
@@ -35,22 +40,50 @@ public class LoggingEvent {
      *
      * @param level   Level
      * @param logger  Logger name
-     * @param message Message
+     * @param pattern Message pattern
      * @param rayArgs Arguments collection, obtained from ray
      * @param args    Arguments collection, provided during event creation
      */
     public LoggingEvent(
             final Level level,
             final String logger,
-            final String message,
+            final String pattern,
             final Args rayArgs,
             final Arg... args
     ) {
         this.millis = System.currentTimeMillis();
         this.level = Objects.requireNonNull(level, "type");
         this.logger = Objects.requireNonNull(logger, "logger");
-        this.message = Objects.requireNonNull(message, "message");
+        this.pattern = Objects.requireNonNull(pattern, "pattern");
         this.args = LazyMergedArgs.of(rayArgs, args);
+    }
+
+    /**
+     * Builds event message using provided placeholder replacer function.
+     *
+     * @param replacer Placeholder replacer
+     * @return Message with placeholders replaced
+     */
+    public String getMessage(final BiFunction<String, Args, String> replacer) {
+        return Objects.requireNonNull(replacer, "replacer").apply(pattern, args);
+    }
+
+    /**
+     * @return Arguments, that contains exceptions
+     */
+    public Collection<ArgException> getExceptions() {
+        // In most cases there will be no errors
+        ArrayList<ArgException> errors = null;
+        for (Arg arg : args) {
+            if (arg instanceof ArgException) {
+                if (errors == null) {
+                    errors = new ArrayList<>();
+                }
+                errors.add((ArgException) arg);
+            }
+        }
+
+        return errors == null ? Collections.emptyList() : errors;
     }
 
     /**
